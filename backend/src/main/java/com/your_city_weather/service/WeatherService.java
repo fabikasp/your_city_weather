c4dff9799a4fad98636059eb52124f39f0ce74c4
fkaspare <fabian.kasparek@stud.htwk-leipzig.de>
2022-11-18 11:00:31 +0100

    Create controller with test service

---
+++ backend/src/main/java/com/your_city_weather/service/WeatherService.java
@@ -0,0 +1,11 @@
+package com.your_city_weather.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherService {
+
+    public String test() {
+        return "test";
+    }
+}
