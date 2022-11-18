76d74dfd78b767cece6db329b60eecaf31bfc718
fkaspare <fabian.kasparek@stud.htwk-leipzig.de>
2022-11-18 13:20:11 +0100

    Complete current weather api

--- backend/src/main/java/com/your_city_weather/controller/Controller.java
+++ backend/src/main/java/com/your_city_weather/controller/Controller.java
@@ -4,6 +4,7 @@ import com.your_city_weather.model.CurrentWeather;
 import com.your_city_weather.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +13,8 @@ public class Controller {
     @Autowired
     private WeatherService weatherService;
 
-    @GetMapping("/current-weather")
-    public CurrentWeather currentWeather() {
-        return weatherService.getCurrentWeatherByCityName("Leipzig");
+    @GetMapping("/current-weather/{city}")
+    public CurrentWeather currentWeather(@PathVariable String city) {
+        return weatherService.getCurrentWeatherByCityName(city);
     }
 }
