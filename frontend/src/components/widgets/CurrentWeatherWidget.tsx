043fbac7128e0a6e15992484317f8a2bad8512ef
fkaspare <fabian.kasparek@stud.htwk-leipzig.de>
2022-11-21 13:14:47 +0100

    Design current weather widget and spinner

--- frontend/src/components/widgets/CurrentWeatherWidget.tsx
+++ frontend/src/components/widgets/CurrentWeatherWidget.tsx
@@ -1,39 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import { CurrentWeather, CURRENT_WEATHER_URL, WEATHER_ICON_URL } from "./types";
+import BeatLoader from "react-spinners/BeatLoader";
 import "./CurrentWeatherWidget.css";
-
-type CurrentWeather = {
-    weatherName: string,
-    weatherDescription: string,
-    weatherIconCode: string,
-    temperature: number,
-    minTemperature: number,
-    maxTemperature: number,
-    pressure: number,
-    humidity: number,
-    visibilityRange: number,
-    windSpeed: number,
-    rainVolume1Hour: number,
-    rainVolume3Hours: number,
-    cloudiness: number,
-    timestamp: string
-};
 
 export const CurrentWeatherWidget = () => {
     const [currentWeather, setCurrentWeather] = useState<CurrentWeather|undefined>(undefined);
-    const [imageUrl, setImageUrl] = useState<string|undefined>(undefined);
-
-    // TODO: Typen und URLs auslagern
-    // TODO: Refactoring, Verbesserung vom ErrorHandling
-    // TODO: Spinner anzeigen
+    const [weatherIconUrl, setWeatherIconUrl] = useState<string|undefined>(undefined);
 
     async function getCurrentWeather(): Promise<CurrentWeather|undefined> {
         try {
             const { data, status } = await Axios.get<CurrentWeather>(
-                'http://localhost:8080/current-weather/DE/Berlin',
-                {
-                    headers: { Accept: 'application/json' },
-                }
+                CURRENT_WEATHER_URL.replace("{countryCode}", "DE").replace("{city}", "Berlin"),
+                { headers: { Accept: 'application/json' } }
             );
 
             if (status != 200) {
@@ -48,17 +27,42 @@ export const CurrentWeatherWidget = () => {
 
     useEffect(() => {
         getCurrentWeather().then((currentWeather) => {
-            setCurrentWeather(currentWeather);
-            setImageUrl("http://openweathermap.org/img/wn/" + currentWeather?.weatherIconCode + "@2x.png");
+            if (currentWeather) {
+                setCurrentWeather(currentWeather);
+                setWeatherIconUrl(WEATHER_ICON_URL.replace("{iconCode}", currentWeather.weatherIconCode));
+            }
         });
     }, []);
 
-    // TODO: Nur anzeigen, wenn currentWeather nicht undefined
-
-    return (
+    const widgetContainer = (
         <div id="current-weather-widget-container">
-            {currentWeather?.cloudiness}
-            <img src={imageUrl} />
+            <div id="current-weather-conclusion-container">
+                <img src={weatherIconUrl} />
+                <div>{currentWeather?.weatherDescription}</div>
+            </div>
+            <div className="current-weather-info-container">
+                <div className="current-weather-upper-infos">Temperatur: <span className="current-weather-data">{currentWeather?.temperature} *C</span></div>
+                <div className="current-weather-upper-infos">Minimaltemperatur: <span className="current-weather-data">{currentWeather?.minTemperature} *C</span></div>
+                <div>Maximaltemperatur: <span className="current-weather-data">{currentWeather?.maxTemperature} *C</span></div>
+            </div>
+            <div className="current-weather-info-container">
+                <div className="current-weather-upper-infos">Bewölkung: <span className="current-weather-data">{currentWeather?.cloudiness ?? 0} %</span></div>
+                <div className="current-weather-upper-infos">Windgeschwindigkeit: <span className="current-weather-data">{currentWeather?.windSpeed ?? 0} m/s</span></div>
+                <div>Regenvolumen: <span className="current-weather-data">{currentWeather?.rainVolume1Hour ?? 0} mm</span></div>
+            </div>
+            <div className="current-weather-info-container">
+                <div className="current-weather-upper-infos">Blickweite: <span className="current-weather-data">{currentWeather?.visibilityRange} m</span></div>
+                <div className="current-weather-upper-infos">Luftfeuchtigkeit: <span className="current-weather-data">{currentWeather?.humidity} %</span></div>
+                <div>Luftdruck: <span className="current-weather-data">{currentWeather?.pressure} hPa</span></div>
+            </div>
         </div>
     );
+
+    const spinnerContainer = (
+        <div id="current-weather-spinner-container">
+            <BeatLoader id="current-weather-spinner" color="#FFFF" loading={true} size={20} />
+        </div>
+    );
+
+    return currentWeather ? widgetContainer : spinnerContainer;
 };
---
+++ frontend/src/components/widgets/types.ts
@@ -0,0 +1,19 @@
+export const CURRENT_WEATHER_URL = "http://localhost:8080/current-weather/{countryCode}/{city}";
+export const WEATHER_ICON_URL = "http://openweathermap.org/img/wn/{iconCode}@4x.png";
+
+export type CurrentWeather = {
+    weatherName: string,
+    weatherDescription: string,
+    weatherIconCode: string,
+    temperature: number,
+    minTemperature: number,
+    maxTemperature: number,
+    pressure: number,
+    humidity: number,
+    visibilityRange: number,
+    windSpeed: number,
+    rainVolume1Hour: number,
+    rainVolume3Hours: number,
+    cloudiness: number,
+    timestamp: string
+};
